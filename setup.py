186844f0762fb57d668e2c2ecb98d338e26506b9
Tyson Littenberg <tyson.littenberg@ligo.org>
2020-12-17 13:37:14 -0600

    adding source code to repo

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lisacattools", # Replace with your own username
+    version="0.0.6",
+    author="James I. Thorpe, Tyson B. Littenberg",
+    author_email="tyson.b.littenberg@nasa.gov",
+    description="A small example package for using LISA catalogs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tlittenberg/ldasoft",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

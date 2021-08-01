1b0248de073fde9b4083fb548c07a3ebb6ce565f
Jean-Christophe Malapert <jean-christophe.malapert@cnes.fr>
2021-08-02 01:35:35 +0200

    Fix the log verbose

--- lisacattools/__init__.py
+++ lisacattools/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2021 - James I. Thorpe, Tyson B. Littenberg, Jean-Christophe
 # Malapert
 #
@@ -15,40 +16,37 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with lisacattools.  If not, see <https://www.gnu.org/licenses/>.
-
-from ._version import (
-    __author__,
-    __author_email__,
-    __copyright__,
-    __description__,
-    __license__,
-    __title__,
-    __name__,
-    __url__,
-    __version__,
-)
-from .utils import FrameEnum
-from .analyze import LisaAnalyse, CatalogAnalysis, HistoryAnalysis
-from .catalog import GWCatalogs, GWCatalog, GWCatalogType
-from .utils import (
-    getSciRD,
-    get_DL,
-    get_Mchirp,
-    confidence_ellipse,
-    convert_ecliptic_to_galactic,
-    convert_galactic_to_cartesian,
-    ellipse_area,
-    HPhist,
-)
-import os
 import logging.config
-from logging import NullHandler
-from .custom_logging import (
-    LogRecord,
-    UtilsLogs,
-)
+import os
 
-logging.getLogger(__name__).addHandler(NullHandler())
+from ._version import __author__  # noqa: F401
+from ._version import __author_email__  # noqa: F401
+from ._version import __copyright__  # noqa: F401
+from ._version import __description__  # noqa: F401
+from ._version import __license__  # noqa: F401
+from ._version import __name__  # noqa: F401
+from ._version import __title__  # noqa: F401
+from ._version import __url__  # noqa: F401
+from ._version import __version__  # noqa: F401
+from .analyze import CatalogAnalysis
+from .analyze import HistoryAnalysis
+from .analyze import LisaAnalyse
+from .catalog import GWCatalog
+from .catalog import GWCatalogs
+from .catalog import GWCatalogType
+from .custom_logging import LogRecord
+from .custom_logging import UtilsLogs
+from .utils import confidence_ellipse
+from .utils import convert_ecliptic_to_galactic
+from .utils import convert_galactic_to_cartesian
+from .utils import ellipse_area
+from .utils import FrameEnum
+from .utils import get_DL
+from .utils import get_Mchirp
+from .utils import getSciRD
+from .utils import HPhist
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 UtilsLogs.addLoggingLevel("TRACE", 15)
 try:
@@ -64,6 +62,16 @@ except Exception as exception:  # pylint: disable=broad-except
     logging.warning("cannot load logging.conf : %s" % exception)
 logging.setLogRecordFactory(LogRecord)  # pylint: disable=no-member
 
+CRITICAL = 50
+FATAL = CRITICAL
+ERROR = 40
+WARNING = 30
+WARN = WARNING
+TRACE = 15
+INFO = 20
+DEBUG = 10
+NOTSET = 0
+OFF = 100
 
 __all__ = [
     "GWCatalogs",
@@ -81,4 +89,14 @@ __all__ = [
     "convert_galactic_to_cartesian",
     "ellipse_area",
     "HPhist",
+    "CRITICAL",
+    "FATAL",
+    "ERROR",
+    "WARNING",
+    "WARN",
+    "TRACE",
+    "INFO",
+    "DEBUG",
+    "NOTSET",
+    "OFF",
 ]
